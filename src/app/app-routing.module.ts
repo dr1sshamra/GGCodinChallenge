f072feae28506ff44ed8aee8150c133ed15e41e8
dr1sshamra <36568019+dr1sshamra@users.noreply.github.com>
2020-03-22 20:21:01 +0100

    add home et reponse component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ReponseComponent } from './reponse/reponse.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component : HomeComponent},
+  {path: 'reponse/:id', component : ReponseComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ReponseComponent } from './reponse/reponse.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ReponseComponent
   ],
   imports: [
     BrowserModule,
