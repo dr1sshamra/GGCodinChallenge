4a4757dabbeff5af0eb0455e6fa3ea767120724d
dr1sshamra <36568019+dr1sshamra@users.noreply.github.com>
2020-03-27 22:33:32 +0100

    Add home + reponse page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { AnimationItem } from 'lottie-web';
+import { AnimationOptions } from 'ngx-lottie';
+import { Subject, Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { QuestionService } from '../services/question.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +11,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  optionsClosed: AnimationOptions = {
+    path: '../../assets/lottie/closed-gift.json',
+  };
 
-  constructor() { }
+  optionsOpened: AnimationOptions = {
+    path: '../../assets/lottie/opened-gift.json',
+    loop: false
+  };
 
-  ngOnInit() {
+  openedVisibility: boolean = false;
+  closedVisibility: boolean = true;
+
+
+  constructor(private router: Router,
+    private ngZone: NgZone,
+    private questionService: QuestionService) {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  closedClicked() {
+    this.toggleBoxes(false)
+  }
+
+  openedComplete() {
+
+    if (this.questionService.isAllAnswered()) {
+      this.navigate(['reponse', 5])
+    } else {
+      let randomQuestion = Math.floor(Math.random() * 4) + 1
+      while(this.AlreadyAnswered(randomQuestion)){
+        randomQuestion = Math.floor(Math.random() * 4) + 1
+      }
+      this.questionService.AddToAnsweredQuestions(randomQuestion)
+      this.navigate(['reponse', randomQuestion])
+    }
+
+  }
+
+  toggleBoxes(val) {
+    this.closedVisibility = val
+    this.openedVisibility = !val
+  }
+
+  public navigate(commands: any[]): void {
+    this.ngZone.run(() => this.router.navigate(commands)).then();
+  }
+
+  AlreadyAnswered(id: number): boolean {
+    let question = this.questionService.questionAnswered.find(q => q == id)
+    if (question == null) {
+      return false
+    }
+    return true
   }
 
 }
--- src/app/reponse/reponse.component.ts
+++ src/app/reponse/reponse.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NO_ERRORS_SCHEMA } from '@angular/core';
+import { QuestionService } from '../services/question.service';
+import { Question } from '../services/question';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-reponse',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reponse.component.scss']
 })
 export class ReponseComponent implements OnInit {
-
-  constructor() { }
+  question: Question
+  constructor(private questionService: QuestionService,
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    let idQuestion = this.activatedRoute.snapshot.paramMap.get('id')
+    this.getAnswer(+idQuestion)
   }
+
+  getAnswer(id: number) {
+    this.questionService.getQuestions().subscribe(res => {
+      this.question = res.find(q => q.id == +id)
+    },
+      errors => {
+        console.log('Oups ! something is wrong. [' + errors + ']')
+      })
+  }
+
+
 
 }
